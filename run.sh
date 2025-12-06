93a1f813454fab59d678cf7be06c0ce539a96052
Pd172944 <prithvidixit05@gmail.com>
2025-12-06 14:01:46 -0800

    instructions on run

--- run.sh
+++ run.sh
@@ -1,2 +1,3 @@
 #!/bin/bash
-/Users/prithvidixit/Desktop/cs194Application/194-2new/venv/bin/python3 -m src.green_agent.server
+# Uses the python interpreter from the current environment (venv)
+python3 -m src.green_agent.server
